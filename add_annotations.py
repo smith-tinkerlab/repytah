d96efe6062c20f7e925f517518052bfbc5c75e03
Jordan M <jormacmoo@gmail.com>
2019-06-19 14:07:51 -0400

    add_annotations rough draft

--- add_annotations.py
+++ add_annotations.py
@@ -16,14 +16,59 @@ RETURNS:
 """
 
 import numpy as np
+import scipy.sparse as sps
 
 def add_annotations(input_mat, song_length):
     
     num_columns = input_mat.shape[0]
     
-    # removes any previous annotations
+    # removes any already present annotation markers
     input_mat[:, 5] = 0
     
     # find where repeats start
+    s_one = input_mat[:,0]
+    s_two = input_mat[:,2]
+    
+    # creates matrix of all repeats
+    s_three = np.ones(num_columns,1)
+    
+    up_tri_mat = sps.coo_matrix((s_three, (s_one, s_two)),
+                                shape = (song_length, song_length)).toarray()
+    
+    low_tri_mat = up_tri_mat.conj().transpose()
+    
+    full_mat = up_tri_mat + low_tri_mat
+    
+    # stitches info from input_mat into a single row
+    song_pattern = stitch_diags(full_mat, 0)
+    
+    # gets maximum of each column
+    sp_max = song_pattern.max(0)
+    
+    # adds annotation markers to pairs of repeats
+    for i in sp_max:
+        pinds = np.nonzero(song_pattern == i)
+        
+        #one if annotation not already marked, 0 if it is
+        check_inds = (input_mat[:,5] == 0)
+        
+        for j in pinds:
+            
+            # finds all starting pairs that contain time step j
+            # and DO NOT have an annotation
+            mark_inds = (s_one == j) + (s_two == j)
+            mark_inds = (mark_inds > 0)
+            mark_inds = check_inds * mark_inds
+            
+            # adds found annotations to the relevant time steps
+            input_mat[:,5] = (input_mat[:,5] + i * mark_inds)
+            
+            # removes pairs of repeats with annotations from consideration
+            check_inds = check_inds - mark_inds
+            
+    (unused, temp_inds) = np.sort(input_mat[:,5])
+    
+    # creates list of annotations
+    anno_list = input_mat[temp_inds,]
     
     return anno_list
