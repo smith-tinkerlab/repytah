245bc144774bfb0f26d97a05c366d61d1e252ae1
Jordan M <jormacmoo@gmail.com>
2019-06-26 15:16:18 -0400

    Add working function

--- stitch_diags.py
+++ stitch_diags.py
@@ -1,47 +1,42 @@
 # -*- coding: utf-8 -*-
-"""
-Stitches information from thresholded diagonal matrix into a single
-    row (where each entry represents a time step and the entry 
-    indicates the group that step is part of)
-
-ARGS:
-    thresh_diags: Binary matrix with 1 at each pair (SI,SJ) and 0
-        elsewhere. WARNING: must be symmetric
-        
-    z_or_n: Binary indicator determining whether NAN or 0 is used
-        for temporary variables. 0 means zeroes and 1 means NANs
-    
-RETURNS:
-    song_pattern: Row where each entry represents a time step and
-        the group that time step is a member of
-    
-"""
 
 import numpy as np
 
-def stitch_diags(thresh_diags, z_or_n):
+def stitch_diags(thresh_diags):
+    
+    """
+    Stitches information from thresholded diagonal matrix into a single
+        row (where each entry represents a time step and the entry 
+        indicates the group that step is part of)
+
+    ARGS:
+        thresh_diags: Binary matrix with 1 at each pair (SI,SJ) and 0
+            elsewhere. WARNING: must be symmetric
+    
+    RETURNS:
+        song_pattern: Row where each entry represents a time step and
+            the group that time step is a member of
+    
+    """
     
     num_rows = thresh_diags.shape[0]
     
-    if z_or_n == 0:
-        p_base = np.zeros((1,num_rows), dtype = int)
-    elif z_or_n == 1:
-        #will I have to do a for loop to change an array of 
-        #zeros into an array of NaNs? 
-    
+    p_base = np.zeros((1,num_rows), dtype = int)
+
     # initializing group number
     pattern_num = 1
     
     col_sum = thresh_diags.sum(axis = 0)
     
     check_inds = col_sum.nonzero()
+    check_inds = check_inds[0]
     
     # creates vector of song length
-    p_mask = np.ones(1, num_rows)
+    p_mask = np.ones((1, num_rows))
     p_out = (col_sum == 0)
     p_mask = p_mask - p_out
     
-    while check_inds.size != 0:
+    while np.size(check_inds) != 0:
         
         # takes first entry in check_inds
         i = check_inds[0]
@@ -52,12 +47,35 @@ def stitch_diags(thresh_diags, z_or_n):
         # finds all time steps that i is close to
         inds = temp_row.nonzero()
         
-        if inds.size != 0:
-            while inds.size != 0:
+        if np.size(inds) != 0:
+            while np.size(inds) != 0:
                 
                 # takes sum of rows corresponding to inds and
                 # multiplies the sums against p_mask
-                c_mat = 
+                c_mat = np.sum(thresh_diags[inds,:], axis = 1)
+                
+                # finds nonzero entries of c_mat
+                c_inds = c_mat.nonzero()
+                
+                # gives all elements of c_inds the same grouping 
+                # number as i
+                p_base[0,c_inds] = pattern_num
+                
+                # removes all used elements of c_inds from
+                # check_inds and p_mask
+                check_inds = np.setdiff1d(check_inds, c_inds)
+                p_mask[0,c_inds] = 0
+                
+                # resets inds to c_inds with inds removed
+                inds = np.setdiff1d(c_inds, inds)
+                
+            # updates grouping number to prepare for next group
+            pattern_num = pattern_num + 1
+            
+        # removes i from check_inds
+        check_inds = np.setdiff1d(check_inds, i)
+        
+    song_pattern = p_base
     
     return song_pattern
 
