182ddca02d92e22e50611bf65a83c964f4a64db9
Jordan M <jormacmoo@gmail.com>
2019-06-27 15:55:05 -0400

    Fix docstring

--- stretch_diags.py
+++ stretch_diags.py
@@ -7,16 +7,20 @@ def stretch_diags(thresh_diags, band_width):
     Stretches entries of matrix of thresholded diagonal onsets
         into diagonals of given length
     
-    ARGS: 
-        thresh_diags: Binary matrix where entries equal to 1 
-            signal the existence of a diagonal
+    ARGS
+    ----
+    thresh_diags: array
+        Binary matrix where entries equal to 1 signal the existence 
+        of a diagonal
     
-        band_width: Length of encoded diagonals
+    band_width: int
+        Length of encoded diagonals
     
-    RETURNS:
-        stretch_diag_mat: Logical matrix with diagonals of length 
-            band_width starting at each entry prescribed in 
-            thresh_diag
+    RETURNS
+    -------
+    stretch_diag_mat: logical array
+        Logical matrix with diagonals of length band_width starting 
+        at each entry prescribed in thresh_diag
     
     """
     # creates size of returned matrix
