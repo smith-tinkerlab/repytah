8822b39ff51ccb3ce13a47e16774b98ef768cfdb
Jordan M <jormacmoo@gmail.com>
2019-07-08 17:13:31 -0400

    Initial code

---
+++ find_complete_list_anno_only.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+import numpy as np
+
+def fcl_anno_only(pair_list, song_length):
+    """
+    Finds annotations for all pairs of repeats found in previous step
+    
+    Args
+    ----
+    pair_list: 
+        list of pairs of repeats
+        WARNING: bandwidths must be in ascending order
+        
+    song_length: int
+        number of audio shingles in song
+        
+    Returns
+    -------
+    out_list:
+        list of pairs of repeats with smaller repeats added and with
+        annotation markers
+        
+    """
+    
+    # find list of unique repeat lengths
+    #bw_found =
+    # remove longest bandwidth row if it is the length of the full song
+    
+    # add annotation markers to each pair of repeats
+    for j in range(b):
+        # isolate pairs of repeats of desired length
+        
+        # get annotations for this bandwidth
+    return out_list
+
