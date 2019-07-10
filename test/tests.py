78ac060bfb5781d235ba858a2f6e6ccfa1c5b2c0
Jordan M <jormacmoo@gmail.com>
2019-07-10 16:00:55 -0400

    Add test folder

---
+++ test/tests.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Test file for aligned-hierarchies
+
+Goal: matlab files used for expected ins and outs like librosa/test
+"""
+
+import pytest
+
+# lets define some test functions
