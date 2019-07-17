af62e45cda632145231a6a6c91bce2eefce3e6b0
Jordan M <jormacmoo@gmail.com>
2019-07-17 14:41:49 -0400

    Complete file2hr_save

--- file2hr_save.py
+++ file2hr_save.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 import numpy as np
+import scipy.io as sio
 
 def file2hr_save(file_in, file_out, num_fv_per_shingle, thresh):
     """
@@ -21,10 +22,9 @@ def file2hr_save(file_in, file_out, num_fv_per_shingle, thresh):
     
     Returns
     -------
-    none. file is saved.
+    none: .mat file is saved.
     """
     # Import file of feature vectors
-    
     with open(file_in, 'r', newline='\n') as f:
         fv_mat = np.loadtxt(f, delimiter = ',')
     
@@ -37,10 +37,14 @@ def file2hr_save(file_in, file_out, num_fv_per_shingle, thresh):
     
     # Extract diagonals from thresholded distance matrix, saving the repeat pairs
     # the diagonals represent
-    all_lst = lightup_lst_with_thresh_bw(thresh_dist_mat, [1:song_length], 0)
+    all_lst = lightup_lst_with_thresh_bw(thresh_dist_mat, range(1, song_length + 1), 0)
     
     # Find smaller repeats contained within larger repeats
     complete_lst = find_complete_list(all_lst, song_length)
+    
+    # Create dictionary of output variables
+    outdict = {}
+    outdict['thresh'] = thresh
     
     if np.size(complete_lst) != 0:
         # Remove groups of repeats that overlap in time
@@ -52,28 +56,33 @@ def file2hr_save(file_in, file_out, num_fv_per_shingle, thresh):
         output_tuple = hierarchical_structure(mat_no_overlaps, key_no_overlaps, song_length)
         (full_key, full_mat_no_overlaps) = output_tuple[1:3]
         
+        outdict['full_key'] = full_key
+        outdict['full_mat_no_overlaps'] = full_mat_no_overlaps
+        
         # Save list of partial representations contatining only the full hierarchical
         # representation for use in comparison code
-        partial_reps = 
-        partial_key = 
-        partial_widths = song_length
-        partial_num_blocks = 
-        num_partials = 1
+        outdict['partial_reps'] = [full_mat_no_overlaps]
+        outdict['partial_key'] = [full_key]
+        outdict['partial_widths'] = song_length
+        outdict['partial_num_blocks'] = np.sum(mat_no_overlaps)
+        outdict['num_partials'] = 1
         
         # Create output file
+        sio.savemat(file_out, outdict)
         
     else:
-        full_key = []
-        full_mat_no_overlaps = []
+        outdict['full_key'] = []
+        outdict['full_mat_no_overlaps'] = []
         
         # Save empty list of partial representations for use in comparison code
-        partial_reps = 
-        partial_key = 
-        partial_widths = []
-        partial_num_blocks = []
-        num_partials = 0
+        outdict['partial_reps'] = []
+        outdict['partial_key'] = []
+        outdict['partial_widths'] = []
+        outdict['partial_num_blocks'] = []
+        outdict['num_partials'] = 0
         
         # Create output file
+        sio.savemat(file_out, outdict)
         
     
     
