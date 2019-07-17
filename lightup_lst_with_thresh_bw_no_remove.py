3b7e8e1fec7805504991a72709ab5c7bb67abf1e
d-nava <44479571+d-nava@users.noreply.github.com>
2019-07-17 11:37:59 -0400

    Add files via upload

---
+++ lightup_lst_with_thresh_bw_no_remove.py
@@ -0,0 +1,138 @@
+import numpy as np
+from scipy import signal
+
+def lightup_lst_with_thresh_band_width_no_remove(thresh_mat,band_width_vec):
+    """
+    Finds all the diagonals present in thresh_mat.
+    
+    Args
+    ----
+    thresh_mat: np.array
+        thresholded matrix that we extract diagonals from
+    
+    band_width_vec: np.array
+        vector of lengths of diagonals to be found
+    
+    Returns
+    -------
+    all_lst: np.array
+        list of pairs of repeats that correspond to diagonals
+        in thresh_mat
+    """
+    # Initialize the input and temporary variables
+    thresh_temp = thresh_mat
+    band_width_vec = band_width_vec
+    b = np.size(band_width_vec,axis=0)
+    
+    int_all = [] # Interval list for non-overlapping pairs
+    sint_all = [] # Interval list for the left side of the overlapping pairs
+    eint_all = [] # Interval list for the right side of the overlapping pairs
+    mint_all = [] # Interval list for the middle of the overlapping pairs if they exist
+    
+    for i in range(1,b+1): # Loop over all possible band_widths
+        # Set current band_width
+        j = b-i+1
+        band_width = band_width_vec[j-1]
+
+        # Search for diagonals of length band_width
+        DDM = signal.convolve2d(thresh_temp[0:,0:],np.eye(band_width),'valid').astype(int)
+
+        # Mark where diagonals of length band_width start
+        thresh_DDM = (DDM == band_width)
+
+        if thresh_DDM.sum() > 0:
+            full_band_width = band_width
+            # 1) Non-Overlaps: Search outside the overlapping shingles
+            # Find the starts that are paired together
+            find_starts = np.nonzero(np.triu(thresh_DDM,full_band_width))
+            start_I = np.array(find_starts[0])
+            start_J = np.array(find_starts[1])
+            num_nonoverlaps = start_I.shape[0]
+
+            # Find the matching ends EI for SI and EJ for SJ
+            end_I = start_I + (full_band_width - 1)
+            end_J = start_J + (full_band_width - 1)
+
+            # List pairs of starts with their ends and the widths of the
+            # non-overlapping intervals
+            int_lst = np.column_stack([start_I,end_I,start_J,end_J,full_band_width * np.ones((num_nonoverlaps,1))]).astype(int)
+
+            # Add the new non-overlapping intervals to the full list of
+            # non-overlapping intervals
+            int_all.append(int_lst)
+
+            # 2) Overlaps: Search only the overlaps in shingles
+            overlap_shingles = np.nonzero(np.tril(np.triu(thresh_DDM,1), (full_band_width - 1)))
+            start_I_overlap = np.array(overlap_shingles[0])
+            start_J_overlap = np.array(overlap_shingles[1])
+            num_overlaps = start_I_overlap.shape[0]
+
+            if num_overlaps > 0:
+                # Since you are checking the overlaps you need to cut these
+                # intervals into pieces: left, right, and middle. NOTE: the
+                # middle interval may NOT exist
+
+                # Vector of 1's that is the length of the number of
+                # overlapping intervals. This is used a lot.
+                ones_no = np.ones((num_overlaps,1)).astype(int)
+
+                # 2a) Left Overlap
+                K = start_J_overlap - start_I_overlap  # NOTE: end_J_overlap - end_I_overlap will also equal this,
+                               # since the intervals that are overlapping are
+                               # the same length. Therefore the "left"
+                               # non-overlapping section is the same length as
+                               # the "right" non-overlapping section. It does
+                               # NOT follow that the "middle" section is equal
+                               # to either the "left" or "right" piece. It is
+                               # possible, but unlikely.
+                sint_lst = np.column_stack([start_I_overlap,(start_J_overlap - ones_no),start_J_overlap,(start_J_overlap + K - ones_no),K]).astype(int)
+                Is = np.argsort(K) # Return the indices that would sort K
+                Is.reshape(np.size(Is), 1)
+                sint_lst = sint_lst[Is,]
+
+                # Add the new left overlapping intervals to the full list
+                # of left overlapping intervals
+                sint_all.append(sint_lst)
+
+                # 2b) Right Overlap
+                end_I_overlap = start_I_overlap + (full_band_width-1)
+                end_J_overlap = start_J_overlap + (full_band_width-1)
+                eint_lst = np.column_stack([(end_I_overlap + ones_no - K), end_I_overlap,(end_I_overlap + ones_no), end_J_overlap, K]).astype(int)
+                Ie = np.argsort(K) # Return the indices that would sort K
+                Ie.reshape(np.size(Ie),1)
+                eint_lst = eint_lst[Ie,:]
+
+                # Add the new right overlapping intervals to the full list of
+                # right overlapping intervals
+                eint_all.append(eint_lst)
+                
+                # 2) Middle Overlap
+                mnds = (end_I_overlap - start_J_overlap - K + ones_no) > 0
+                SIm = start_J_overlap * (mnds)
+                EIm = (end_I_overlap*(mnds) - K*(mnds))
+                SJm = (start_J_overlap*(mnds) + K*(mnds))
+                EJm = end_I_overlap*(mnds)
+                Km = (end_I_overlap*(mnds) - start_J_overlap*(mnds) - K*(mnds) + ones_no*(mnds))
+            
+                if mnds.sum() > 0:
+                    mint_lst = np.column_stack([SIm,EIm,SJm,EJm,Km])
+                    Im = np.argsort(Km)
+                    Im.reshape(np.size(Im),1)
+                    mint_lst = mint_lst[Im,:]
+
+                    # Add the new middle overlapping intervals to the full list
+                    # of middle overlapping intervals
+                    mint_all.append(mint_lst)
+
+        if thresh_temp.sum() == 0:
+            break 
+        
+    out_lst = sint_all + eint_all + mint_all + int_all
+    all_lst = filter(None,out_lst)
+
+    if out_lst is not None:
+        all_lst = np.vstack(out_lst)
+    else:
+        all_lst = np.array([])
+
+    return all_lst
