770150268dcb8ffca7b87a258bd98ba9e832c6e3
Jordan M <jormacmoo@gmail.com>
2019-07-18 14:07:04 -0400

    Add module

---
+++ aligned_hierarchies.py
@@ -0,0 +1,75 @@
+# -*- coding: utf-8 -*-
+import numpy as np
+import scipy.spatialdistance as spd
+import
+
+def add_annotations(input_mat, song_length):
+    """
+    Adds annotations to pairs of repeats in input matrix
+
+    Args
+    ----
+    input_mat: array
+        list of pairs of repeats. The first two columns refer to 
+        the first repeat of the pair. The third and fourth columns refer
+        to the second repeat of the pair. The fifth column refers to the
+        repeat lengths. The sixth column contains any previous annotations,
+        which will be removed.
+        
+    song_length: int
+        number of audio shingles in the song.
+    
+    Returns
+    -------
+    anno_list: array
+        list of pairs of repeats with annotations marked
+    """
+    num_rows = input_mat.shape[0]
+    
+    # Removes any already present annotation markers
+    input_mat[:, 5] = 0
+    
+    # Find where repeats start
+    s_one = input_mat[:,0]
+    s_two = input_mat[:,2]
+    
+    # Creates matrix of all repeats
+    s_three = np.ones((num_rows,), dtype = int)
+    
+    up_tri_mat = sps.coo_matrix((s_three, (s_one, s_two)),
+                                shape = (song_length, song_length)).toarray()
+    
+    low_tri_mat = up_tri_mat.conj().transpose()
+    
+    full_mat = up_tri_mat + low_tri_mat
+    
+    # Stitches info from input_mat into a single row
+    song_pattern = find_song_pattern(full_mat)
+    
+    # Adds annotation markers to pairs of repeats
+    for i in song_pattern[0]:
+        pinds = np.nonzero(song_pattern == i)
+        
+        #One if annotation not already marked, 0 if it is
+        check_inds = (input_mat[:,5] == 0)
+        
+        for j in pinds[1]:
+            
+            # Finds all starting pairs that contain time step j
+            # and DO NOT have an annotation
+            mark_inds = (s_one == j) + (s_two == j)
+            mark_inds = (mark_inds > 0)
+            mark_inds = check_inds * mark_inds
+            
+            # Adds found annotations to the relevant time steps
+            input_mat[:,5] = (input_mat[:,5] + i * mark_inds)
+            
+            # Removes pairs of repeats with annotations from consideration
+            check_inds = check_inds ^ mark_inds
+     
+    temp_inds = np.argsort(input_mat[:,5])
+    
+    # Creates list of annotations
+    anno_list = input_mat[temp_inds,]
+    
+    return anno_list
