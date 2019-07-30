2e31e03320764900a977e1e9d395ef4eb235ad34
Denise Nava <denise.nava@Denises-MacBook-Pro.local>
2019-07-30 14:50:07 -0400

    Utilities update

--- separated-functions/utilities.py
+++ separated-functions/utilities.py
@@ -7,20 +7,20 @@ This script when imported as a module allows search.py, disassemble.py and
 assemble.py in the ah package to run smoothly. 
 
 This file contains the following functions:
-    
-    * reconstruct_full_block - Creates a record of when pairs of repeated
-    structures occur, from the first beat in the song to the last beat of the
-    song. Pairs of repeated structures are marked with 1's. 
+
+    * add_annotations - Adds annotations to each pair of repeated structures 
+    according to their length and order of occurence. 
+
+    * create_sdm - Creates a self-dissimilarity matrix; this matrix is found 
+    by creating audio shingles from feature vectors, and finding cosine 
+    distance between shingles. 
 
     * find_initial_repeats - Finds all diagonals present in thresh_mat, 
     removing each diagonal as it is found.
     
-    * add_annotations - Adds annotations to each pair of repeated structures 
-    according to their length and order of occurence. 
-    
-    * create_sdm - Creates a self-dissimilarity matrix; this matrix is found 
-    by creating audio shingles from feature vectors, and finding cosine 
-    distance between shingles. 
+    * reconstruct_full_block - Creates a record of when pairs of repeated
+    structures occur, from the first beat in the song to the last beat of the
+    song. Pairs of repeated structures are marked with 1's. 
     
     * reformat - Transforms a binary matrix representation of when repeats 
     occur in a song into a list of repeated structures detailing the length
@@ -29,104 +29,127 @@ This file contains the following functions:
 """
 
 import numpy as np
-
 from scipy import signal
-
 import scipy.sparse as sps
-
 import scipy.spatial.distance as spd
 
-def reconstruct_full_block(pattern_mat, pattern_key): 
+def add_annotations(input_mat, song_length):
     """
-    Creates a record of when pairs of repeated structures occur, from the 
-    first beat in the song to the end. This record is a binary matrix with a 
-    block of 1's for each repeat encoded in pattern_mat whose length 
-    is encoded in pattern_key
+    Adds annotations to the pairs of repeats in input_mat; Annotations depend 
+    on length of repeats and the time that repeats occur in song
+
+    Args
+    ----
+    input_mat: np.array
+        list of pairs of repeats. The first two columns refer to 
+        the first repeat of the pair. The third and fourth columns refer
+        to the second repeat of the pair. The fifth column refers to the
+        repeat lengths. The sixth column contains any previous annotations,
+        which will be removed.
+        
+    song_length: int
+        number of audio shingles in the song.
+    
+    Returns
+    -------
+    anno_list: array
+        list of pairs of repeats with annotations marked. 
+    """
+    num_rows = input_mat.shape[0]
+    
+    # Removes any already present annotation markers
+    input_mat[:, 5] = 0
+    
+    # Find where repeats start
+    s_one = input_mat[:,0]
+    s_two = input_mat[:,2]
+    
+    # Creates matrix of all repeats
+    s_three = np.ones((num_rows,), dtype = int)
+    
+    up_tri_mat = sps.coo_matrix((s_three, 
+                                 (s_one, s_two)), shape = (song_length + 1, 
+                                 song_length + 1)).toarray()
+    
+    low_tri_mat = up_tri_mat.conj().transpose()
+    
+    full_mat = up_tri_mat + low_tri_mat
+    
+    # Stitches info from input_mat into a single row
+    song_pattern = __find_song_pattern(full_mat)
+    
+    # Restructures song_pattern
+    song_pattern = song_pattern[:,:-1]
+    song_pattern = np.insert(song_pattern, 0, 0, axis=1)
+    
+    # Adds annotation markers to pairs of repeats
+    for i in song_pattern[0]:
+        pinds = np.nonzero(song_pattern == i)
+        
+        #One if annotation not already marked, 0 if it is
+        check_inds = (input_mat[:,5] == 0)
+        
+        for j in pinds[1]:
+            
+            # Finds all starting pairs that contain time step j
+            # and DO NOT have an annotation
+            mark_inds = (s_one == j) + (s_two == j)
+            mark_inds = (mark_inds > 0)
+            mark_inds = check_inds * mark_inds
+            
+            # Adds found annotations to the relevant time steps
+            input_mat[:,5] = (input_mat[:,5] + i * mark_inds)
+            
+            # Removes pairs of repeats with annotations from consideration
+            check_inds = check_inds ^ mark_inds
+     
+    temp_inds = np.argsort(input_mat[:,5])
+    
+    # Creates list of annotations
+    anno_list = input_mat[temp_inds,]
+    
+    return anno_list
+
+def create_sdm(fv_mat, num_fv_per_shingle):
+    """
+    Creates self-dissimilarity matrix; this matrix is found by creating audio 
+    shingles from feature vectors, and finding cosine distance between 
+    shingles
     
     Args
     ----
-    pattern_mat: np.array
-        binary matrix with 1's where repeats begin 
-        and 0's otherwise
-     
-    pattern_key: np.array
-        vector containing the lengths of the repeats 
-        encoded in each row of pattern_mat
-
+    fv_mat: np.array
+        matrix of feature vectors where each column is a timestep and each row
+        includes feature information i.e. an array of 144 columns/beats and 12
+        rows corresponding to chroma values
+        
+    num_fv_per_shingle: int
+        number of feature vectors per audio shingle
+    
     Returns
     -------
-    pattern_block: np.array
-        binary matrix representation for pattern_mat 
-        with blocks of 1's equal to the length's 
-        prescribed in pattern_key
+    self_dissim_mat: np.array 
+        self dissimilarity matrix with paired cosine distances between 
+        shingles
     """
-    #First, find number of beats (columns) in pattern_mat: 
-    #Check size of pattern_mat (in cases where there is only 1 pair of
-    #repeated structures)
-    if (pattern_mat.ndim == 1): 
-        #Convert a 1D array into 2D array 
-        #From:
-        #https://stackoverflow.com/questions/3061761/numpy-array-dimensions
-        pattern_mat = pattern_mat[None, : ]
-        #Assign number of beats to sn 
-        sn = pattern_mat.shape[1]
-    else: 
-        #Assign number of beats to sn 
-        sn = pattern_mat.shape[1]
-        
-    #Assign number of repeated structures (rows) in pattern_mat to sb 
-    sb = pattern_mat.shape[0]
-    
-    #Pre-allocating a sn by sb array of zeros 
-    pattern_block = np.zeros((sb,sn)).astype(int)  
-    
-    #Check if pattern_key is in vector row 
-    if pattern_key.ndim != 1: 
-        #Convert pattern_key into a vector row 
-        length_vec = np.array([])
-        for i in pattern_key:
-            length_vec = np.append(length_vec, i).astype(int)
-    else: 
-        length_vec = pattern_key 
-    
-    for i in range(sb):
-        #Retrieve all of row i of pattern_mat 
-        repeated_struct = pattern_mat[i,:]
-    
-        #Retrieve the length of the repeats encoded in row i of pattern_mat 
-        length = length_vec[i]
-    
-        #Pre-allocate a section of size length x sn for pattern_block
-        sub_section = np.zeros((length, sn))
-    
-        #Replace first row in block_zeros with repeated_structure 
-        sub_section[0,:] = repeated_struct
-        
-        #Creates pattern_block: Sums up each column after sliding repeated 
-        #sastructure i to the right bw - 1 times 
-        for b in range(2, length + 1): 
-    
-            #Retrieve repeated structure i up to its (1 - b) position 
-            sub_struct_a = repeated_struct[0:(1 - b)]
-    
-            #Row vector with number of entries not included in sub_struct_a  
-            sub_struct_b = np.zeros((1,( b  - 1)))
-    
-            #Append sub_struct_b in front of sub_struct_a 
-            new_struct = np.append(sub_struct_b, sub_struct_a)
-            
-            #Replace part of sub_section with new_struct 
-            sub_section[b - 1,:] = new_struct
-    
-    #Replaces part of pattern_block with the sums of each column in 
-    #sub_section 
-    pattern_block[i,:] = np.sum(sub_section, axis = 0)
-    
-    return pattern_block
-    
-#line 217: 
-#https://stackoverflow.com/questions/2828059/sorting-arrays-in-np-by-column
+    [num_rows, num_columns] = fv_mat.shape
+    if num_fv_per_shingle == 1:
+        mat_as = fv_mat
+    else:
+        mat_as = np.zeros(((num_rows * num_fv_per_shingle),
+                           (num_columns - num_fv_per_shingle + 1)))
+        for i in range(1, num_fv_per_shingle+1):
+            # Use feature vectors to create an audio shingle
+            # for each time step and represent these shingles
+            # as vectors by stacking the relevant feature
+            # vectors on top of each other
+            mat_as[((i-1)*num_rows+1)-1:(i*num_rows), : ] = fv_mat[:, 
+                   i-1:(num_columns- num_fv_per_shingle + i)]
 
+    sdm_row = spd.pdist(mat_as.T, 'cosine')
+    self_dissim_mat = spd.squareform(sdm_row)
+    return self_dissim_mat
+  
 def find_initial_repeats(thresh_mat, bandwidth_vec, thresh_bw):
     """
     Identifies all repeated structures in a sequential data stream which are 
@@ -328,124 +351,96 @@ def find_initial_repeats(thresh_mat, bandwidth_vec, thresh_bw):
     #return final list
     return all_lst
 
-
-def add_annotations(input_mat, song_length):
+def reconstruct_full_block(pattern_mat, pattern_key): 
     """
-    Adds annotations to the pairs of repeats in input_mat; Annotations depend 
-    on length of repeats and the time that repeats occur in song
-
+    Creates a record of when pairs of repeated structures occur, from the 
+    first beat in the song to the end. This record is a binary matrix with a 
+    block of 1's for each repeat encoded in pattern_mat whose length 
+    is encoded in pattern_key
+    
     Args
     ----
-    input_mat: np.array
-        list of pairs of repeats. The first two columns refer to 
-        the first repeat of the pair. The third and fourth columns refer
-        to the second repeat of the pair. The fifth column refers to the
-        repeat lengths. The sixth column contains any previous annotations,
-        which will be removed.
-        
-    song_length: int
-        number of audio shingles in the song.
-    
-    Returns
-    -------
-    anno_list: array
-        list of pairs of repeats with annotations marked. 
-    """
-    num_rows = input_mat.shape[0]
-    
-    # Removes any already present annotation markers
-    input_mat[:, 5] = 0
-    
-    # Find where repeats start
-    s_one = input_mat[:,0]
-    s_two = input_mat[:,2]
-    
-    # Creates matrix of all repeats
-    s_three = np.ones((num_rows,), dtype = int)
-    
-    up_tri_mat = sps.coo_matrix((s_three, 
-                                 (s_one, s_two)), shape = (song_length + 1, 
-                                 song_length + 1)).toarray()
-    
-    low_tri_mat = up_tri_mat.conj().transpose()
-    
-    full_mat = up_tri_mat + low_tri_mat
-    
-    # Stitches info from input_mat into a single row
-    song_pattern = __find_song_pattern(full_mat)
-    
-    # Restructures song_pattern
-    song_pattern = song_pattern[:,:-1]
-    song_pattern = np.insert(song_pattern, 0, 0, axis=1)
-    
-    # Adds annotation markers to pairs of repeats
-    for i in song_pattern[0]:
-        pinds = np.nonzero(song_pattern == i)
-        
-        #One if annotation not already marked, 0 if it is
-        check_inds = (input_mat[:,5] == 0)
-        
-        for j in pinds[1]:
-            
-            # Finds all starting pairs that contain time step j
-            # and DO NOT have an annotation
-            mark_inds = (s_one == j) + (s_two == j)
-            mark_inds = (mark_inds > 0)
-            mark_inds = check_inds * mark_inds
-            
-            # Adds found annotations to the relevant time steps
-            input_mat[:,5] = (input_mat[:,5] + i * mark_inds)
-            
-            # Removes pairs of repeats with annotations from consideration
-            check_inds = check_inds ^ mark_inds
+    pattern_mat: np.array
+        binary matrix with 1's where repeats begin 
+        and 0's otherwise
      
-    temp_inds = np.argsort(input_mat[:,5])
-    
-    # Creates list of annotations
-    anno_list = input_mat[temp_inds,]
-    
-    return anno_list
+    pattern_key: np.array
+        vector containing the lengths of the repeats 
+        encoded in each row of pattern_mat
 
-def create_sdm(fv_mat, num_fv_per_shingle):
-    """
-    Creates self-dissimilarity matrix; this matrix is found by creating audio 
-    shingles from feature vectors, and finding cosine distance between 
-    shingles
-    
-    Args
-    ----
-    fv_mat: np.array
-        matrix of feature vectors where each column is a timestep and each row
-        includes feature information i.e. an array of 144 columns/beats and 12
-        rows corresponding to chroma values
-        
-    num_fv_per_shingle: int
-        number of feature vectors per audio shingle
-    
     Returns
     -------
-    self_dissim_mat: np.array 
-        self dissimilarity matrix with paired cosine distances between 
-        shingles
+    pattern_block: np.array
+        binary matrix representation for pattern_mat 
+        with blocks of 1's equal to the length's 
+        prescribed in pattern_key
     """
-    [num_rows, num_columns] = fv_mat.shape
-    if num_fv_per_shingle == 1:
-        mat_as = fv_mat
-    else:
-        mat_as = np.zeros(((num_rows * num_fv_per_shingle),
-                           (num_columns - num_fv_per_shingle + 1)))
-        for i in range(1, num_fv_per_shingle+1):
-            # Use feature vectors to create an audio shingle
-            # for each time step and represent these shingles
-            # as vectors by stacking the relevant feature
-            # vectors on top of each other
-            mat_as[((i-1)*num_rows+1)-1:(i*num_rows), : ] = fv_mat[:, 
-                   i-1:(num_columns- num_fv_per_shingle + i)]
-
-    sdm_row = spd.pdist(mat_as.T, 'cosine')
-    self_dissim_mat = spd.squareform(sdm_row)
-    return self_dissim_mat
-  
+    #First, find number of beats (columns) in pattern_mat: 
+    #Check size of pattern_mat (in cases where there is only 1 pair of
+    #repeated structures)
+    if (pattern_mat.ndim == 1): 
+        #Convert a 1D array into 2D array 
+        #From:
+        #https://stackoverflow.com/questions/3061761/numpy-array-dimensions
+        pattern_mat = pattern_mat[None, : ]
+        #Assign number of beats to sn 
+        sn = pattern_mat.shape[1]
+    else: 
+        #Assign number of beats to sn 
+        sn = pattern_mat.shape[1]
+        
+    #Assign number of repeated structures (rows) in pattern_mat to sb 
+    sb = pattern_mat.shape[0]
+    
+    #Pre-allocating a sn by sb array of zeros 
+    pattern_block = np.zeros((sb,sn)).astype(int)  
+    
+    #Check if pattern_key is in vector row 
+    if pattern_key.ndim != 1: 
+        #Convert pattern_key into a vector row 
+        length_vec = np.array([])
+        for i in pattern_key:
+            length_vec = np.append(length_vec, i).astype(int)
+    else: 
+        length_vec = pattern_key 
+    
+    for i in range(sb):
+        #Retrieve all of row i of pattern_mat 
+        repeated_struct = pattern_mat[i,:]
+    
+        #Retrieve the length of the repeats encoded in row i of pattern_mat 
+        length = length_vec[i]
+    
+        #Pre-allocate a section of size length x sn for pattern_block
+        sub_section = np.zeros((length, sn))
+    
+        #Replace first row in block_zeros with repeated_structure 
+        sub_section[0,:] = repeated_struct
+        
+        #Creates pattern_block: Sums up each column after sliding repeated 
+        #sastructure i to the right bw - 1 times 
+        for b in range(2, length + 1): 
+    
+            #Retrieve repeated structure i up to its (1 - b) position 
+            sub_struct_a = repeated_struct[0:(1 - b)]
+    
+            #Row vector with number of entries not included in sub_struct_a  
+            sub_struct_b = np.zeros((1,( b  - 1)))
+    
+            #Append sub_struct_b in front of sub_struct_a 
+            new_struct = np.append(sub_struct_b, sub_struct_a)
+            
+            #Replace part of sub_section with new_struct 
+            sub_section[b - 1,:] = new_struct
+    
+    #Replaces part of pattern_block with the sums of each column in 
+    #sub_section 
+    pattern_block[i,:] = np.sum(sub_section, axis = 0)
+    
+    return pattern_block
+    
+#line 217: 
+#https://stackoverflow.com/questions/2828059/sorting-arrays-in-np-by-column
 
 def reformat(pattern_mat, pattern_key):
     """
@@ -509,10 +504,3 @@ def reformat(pattern_mat, pattern_key):
                 info_mat[r, 4] = pattern_key[r]
                 
     return info_mat 
-
-
-
-
-
-
-
