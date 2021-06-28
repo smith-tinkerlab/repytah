a4af6ae831a1814ee183799d5dd268786f0bc417
Thu Tran <tntran@smith.edu>
2021-06-28 14:28:08 +0700

    Delete old csv calls

--- aligned-hierarchies/example.py
+++ aligned-hierarchies/example.py
@@ -33,8 +33,6 @@ def csv_to_aligned_hierarchies(file_in, file_out, num_fv_per_shingle, thresh):
     none: .mat file is saved. Contains variables created for aligned hierarchies. 
     """
     # Import file of feature vectors
-    # with open(file_in, 'r', newline='\n') as f:
-    #     fv_mat = np.loadtxt(f, delimiter = ',')
     fv_mat = file_in
     
     # Get pairwise distance matrix/self dissimilarity matrix using cosine distance
@@ -98,7 +96,6 @@ def csv_to_aligned_hierarchies(file_in, file_out, num_fv_per_shingle, thresh):
 
         
 # Run on example file
-# file_in = "input.csv"
 file_in = pd.read_csv(os.path.join(os.path.dirname(__file__), "../input.csv")).to_numpy()
 file_out = "hierarchical_out_file.mat"
 num_fv_per_shingle = 3
