09b0da389010966601e6910fc709f67af1ae381e
Marium Tapal <mariumtapal@gmail.com>
2021-06-30 13:00:17 +0500

    update setup

--- setup.py
+++ setup.py
@@ -4,15 +4,15 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setup(
-    name='mirah',  # Required
-    version='0.0.0-alpha',  # Required
+    name='mirah',  
+    version='0.0.0-alpha',
     description='mirah: Python package for building Aligned Hierarchies for music-based data streams.',  # Optional
-    long_description=long_description,  # Optional
-    long_description_content_type='text/markdown',  # Optional
-    url='https://github.com/smith-tinkerlab/aht',  # Optional
-    author='Katherine M. Kinnaird, Smith Tinkerlab',  # Optional
-    author_email='kkinnaird@smith.edu',  # Optional
-    classifiers=[  # Optional
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/smith-tinkerlab/aht',
+    author='Katherine M. Kinnaird, Smith Tinkerlab',
+    author_email='kkinnaird@smith.edu',
+    classifiers=[
         'Development Status :: 1 - Planning',
         'Intended Audience :: Developers',
         'Topic :: Software Development :: Build Tools',
@@ -22,18 +22,28 @@ setup(
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
-        'Programming Language :: Python :: 3 :: Only',
     ],
-    package_dir={'': 'src'},  # Optional
-    packages=find_packages(where='src'),  # Required
+    package_dir={'': 'src'},
+    packages=find_packages(where='src'),
     python_requires='>=3.6, <4',
-    install_requires=['peppercorn'],  # Optional
-    extras_require={  # Optional
+    install_requires=[
+        'numpy >= 1.15.0',
+        'scipy.io', 
+        'pandas',
+        'matplotlib'
+    ],
+    extras_require={
         'dev': ['check-manifest'],
         'test': ['coverage'],
     },
-    project_urls={  # Optional
+    project_urls={ 
         'Bug Reports': 'https://github.com/smith-tinkerlab/ah/issues',
         'Source': 'https://github.com/smith-tinkerlab/ah',
     },
+    package_data={'mirah': ['input.csv']},
+    include_package_data=True,
+    license_file = ('LICENSE.md'),
+    license='MIT',
+    license_file_content_type = 'text/markdown',
+
 )
