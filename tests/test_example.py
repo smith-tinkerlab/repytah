6622c97f16ced5b599eeaca6786d26dc46ab7656
Marium Tapal <mariumtapal@gmail.com>
2021-07-06 16:45:52 +0500

    Update test_example.py

--- tests/test_example.py
+++ tests/test_example.py
@@ -20,12 +20,14 @@ class TestExample(unittest.TestCase):
         Tests that nothing is returned 
         """
 
-        file_in = pd.read_csv(os.path.join(os.path.dirname(__file__), "../input.csv")).to_numpy()
+        file_in = pd.read_csv(os.path.join(os.path.dirname(__file__),
+                                           "../input.csv")).to_numpy()
         file_out = "tests/hierarchical_out_file.mat"
         num_fv_per_shingle = 3
         thresh = 0.01
 
-        output = csv_to_aligned_hierarchies(file_in, file_out, num_fv_per_shingle, thresh)
+        output = csv_to_aligned_hierarchies(file_in, file_out, 
+                                            num_fv_per_shingle, thresh)
 
         self.assertIs(output, None, "Should be none")
 
@@ -34,12 +36,14 @@ class TestExample(unittest.TestCase):
         Tests that a file is saved 
         """
 
-        file_in = pd.read_csv(os.path.join(os.path.dirname(__file__), "../input.csv")).to_numpy()
+        file_in = pd.read_csv(os.path.join(os.path.dirname(__file__),
+                                           "../input.csv")).to_numpy()
         file_out = "tests/hierarchical_out_file.mat"
         num_fv_per_shingle = 3
         thresh = 0.01
 
-        csv_to_aligned_hierarchies(file_in, file_out, num_fv_per_shingle, thresh)
+        csv_to_aligned_hierarchies(file_in, file_out, num_fv_per_shingle, 
+                                   thresh)
 
         self.assertTrue(path.exists("tests/hierarchical_out_file.mat"))
 
@@ -48,14 +52,17 @@ class TestExample(unittest.TestCase):
         Tests that the file saved isn't empty
         """
 
-        file_in = pd.read_csv(os.path.join(os.path.dirname(__file__), "../input.csv")).to_numpy()
+        file_in = pd.read_csv(os.path.join(os.path.dirname(__file__),
+                                           "../input.csv")).to_numpy()
         file_out = "tests/hierarchical_out_file.mat"
         num_fv_per_shingle = 3
         thresh = 0.01
 
-        csv_to_aligned_hierarchies(file_in, file_out, num_fv_per_shingle, thresh)
+        csv_to_aligned_hierarchies(file_in, file_out, num_fv_per_shingle, 
+                                   thresh)
 
-        self.assertFalse(os.stat("tests/hierarchical_out_file.mat").st_size == 0)
+        self.assertFalse(
+            os.stat("tests/hierarchical_out_file.mat").st_size == 0)
     
 
 if __name__ == '__main__':
