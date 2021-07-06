0a085b752ba83912948bb2c69a1a7ce20b221b77
Marium Tapal <mariumtapal@gmail.com>
2021-07-06 14:22:06 +0500

    change comment

--- repytah/__init__.py
+++ repytah/__init__.py
@@ -4,7 +4,7 @@ name = "repytah"
 from .version import version as __version__
 from .version import show_versions
 
-# from .version import __version__
+# import submodules
 from .search import *
 from .utilities import *
 from .transform import *
