53834a3e3c89c421db4bcad6485c6b07ae0ce2bc
Marium Tapal <mariumtapal@gmail.com>
2021-07-06 14:20:11 +0500

    add version file

--- repytah/__init__.py
+++ repytah/__init__.py
@@ -1,6 +1,10 @@
 name = "repytah"
 
-#from .version import __version__
+# import version module
+from .version import version as __version__
+from .version import show_versions
+
+# from .version import __version__
 from .search import *
 from .utilities import *
 from .transform import *
