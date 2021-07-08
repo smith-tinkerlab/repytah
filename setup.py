cc300b2d126e4b641acb5cffca661cd22b71da6b
Marium Tapal <mariumtapal@gmail.com>
2021-07-08 13:21:28 +0500

    oops

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ setup(
     install_requires=[
         'numpy >= 1.15.0',
         'scipy >= 1.0.0', 
-        'pandas' >= '1.0.0',
+        'pandas >= 1.0.0',
         'matplotlib >= 3.3.0'
     ],
     extras_require={
