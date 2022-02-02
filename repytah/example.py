10709c4533f9ec7bb9a3977d9ecf78a63d14c416
Thu Tran <tntran@smith.edu>
2022-02-02 17:48:48 -0500

    Write functions to visualize pairs of repeats in all_lst and complete_lst, implement in vignette

--- repytah/example.py
+++ repytah/example.py
@@ -2,6 +2,8 @@ import scipy.io as sio
 import numpy as np
 import pkg_resources
 import pandas as pd
+import matplotlib.pyplot as plt
+import random
 
 from .utilities import create_sdm, find_initial_repeats
 from .search import find_complete_list
@@ -76,8 +78,8 @@ def csv_to_aligned_hierarchies(file_in, file_out, num_fv_per_shingle, thresh):
     >>> file_in = pd.read_csv(os.path.join(os.path.dirname(__file__),
                               "../input.csv")).to_numpy()
     >>> file_out = "hierarchical_out_file.mat"
-    >>> num_fv_per_shingle = 3
-    >>> thresh = 0.01
+    >>> num_fv_per_shingle = 12
+    >>> thresh = 0.02
     >>> csv_to_aligned_hierarchies(file_in, file_out,
                                    num_fv_per_shingle, thresh)
                                    
@@ -146,3 +148,75 @@ def csv_to_aligned_hierarchies(file_in, file_out, num_fv_per_shingle, thresh):
         
         # Create the output file
         sio.savemat(file_out, outdict)
+
+def visualize_all_lst(all_lst, thresh_dist_mat):
+    """
+    Produce a visualization to highlight all pairs of repeats.
+
+    Args
+    ----
+    all_lst : np.array
+        Pairs of repeats that correspond to diagonals in thresh_mat.
+
+    thresh_dist_mat : np.ndarray
+        Thresholded dissimilarity matrix that we extract diagonals from.
+    
+    
+    Returns
+    -------
+    none : A visualization of all pairs of repeated is produced.
+
+    """
+
+    # Produce a visualization of the SDM
+    SDM = plt.imshow(thresh_dist_mat, cmap="Greys")
+    colors = ["red", "blue", "orange"]
+
+    max_length = all_lst[len(all_lst) - 1][4]
+
+    for pairs_of_repeat in all_lst:
+        first_repeat = pairs_of_repeat[0:2]
+        second_repeat = pairs_of_repeat[2:4]
+        repeat_length = pairs_of_repeat[4]
+
+        if (repeat_length > 1 and repeat_length != max_length) :
+            plt.plot(first_repeat, second_repeat, color = random.choice(colors))
+
+    plt.show()
+
+def visualize_complete_lst(complete_lst, thresh_dist_mat):
+    """
+    Produce a visualization to highlight all pairs of smaller repeats
+    that are contained in larger diagonals.
+
+    Args
+    ----
+    complete_lst : np.ndarray 
+        List of pairs of repeats with smaller repeats added.
+
+    thresh_dist_mat : np.ndarray
+        Thresholded dissimilarity matrix that we extract diagonals from.
+    
+    
+    Returns
+    -------
+    none : A visualization of all pairs of repeated smaller repeats
+           that are contained in larger diagonals is produced.
+
+
+    """
+
+    # Produce a visualization of the SDM
+    SDM = plt.imshow(thresh_dist_mat, cmap="Greys")
+    colors = ["red", "blue", "orange", "green", "purple", "yellow"]
+
+    for pairs_of_repeat in complete_lst:
+        first_repeat = pairs_of_repeat[0:2]
+        second_repeat = pairs_of_repeat[2:4]
+        repeat_length = pairs_of_repeat[4]
+
+        if (repeat_length > 1):
+            plt.plot(first_repeat, second_repeat, color = random.choice(colors))
+
+    plt.show()
+
