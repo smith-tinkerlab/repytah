836b62a09dc46eb898a787c22b7129da3abedf9b
Katherine M. Kinnaird <kkinnaird@smith.edu>
2023-05-05 10:39:48 +0100

    updating version in setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='repytah',
-    version='0.1.0',
+    version='0.1.1',
     description='Python package for building Aligned Hierarchies for sequential data streams',
     long_description=long_description,
     long_description_content_type='text/markdown',
